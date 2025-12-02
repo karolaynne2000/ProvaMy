63a05db32a4fb5f2b7c68fa724923802d693d93f
Aluno <0001153385@senaimgaluno.com.br>
2025-12-02 08:29:45 -0300

    modificações

--- Novas modificações no My.sql
+++ Novas modificações no My.sql
@@ -17,3 +17,54 @@ show tables;
 describe cliente;
 
 select * from cliente where bairrocliente='Santana';
+
+show tables;
+describe Cliente;
+select * from Cliente;
+
+select  codigoCliente,nomeCliente from cliente where bairroCliente='Santana';
+select  codigoCliente,nomeCliente from cliente where codigoCliente>3;
+
+
+alter table cliente add column profCliente varchar(45);
+alter table cliente add emailCliente varchar(45);
+
+
+update cliente set emailCliente='karolsilva@gmail.com' where codigocliente=1;
+update cliente set emailCliente='Liasantos@gmail.com' where codigocliente=2;
+update cliente set emailCliente='Ellaalves@gmail.com' where codigocliente=3;
+update cliente set emailCliente='Cauãlima@gmail.com' where codigocliente=4;
+update cliente set emailCliente='Nicolasalmeida@gmail.com' where codigocliente=5;
+
+
+
+update cliente set profCliente='Dentista' where codigocliente=1;
+update cliente set profCliente='Adevogada' where codigocliente=2;
+update cliente set profCliente='Bombeira' where codigocliente=3;
+update cliente set profCliente='Professor' where codigocliente=4;
+update cliente set profCliente='Eletricista' where codigocliente=5;
+
+
+DELETE FROM Cliente WHERE nomeCliente = 'Karolaynne' AND codigoCliente = 1;
+update cliente set nomeCliente = 'Karol' where codigoCliente=1;
+delete from cliente where codigoCliente=4;
+
+alter table cliente add column cpfCliente int;
+alter table cliente add column datanascCliente date;
+alter table cliente drop datanascCliente;
+
+select * from Cliente;
+
+update cliente set cpfCliente=1234755678 where codigocliente=1;
+update cliente set cpfCliente=67589893627 where codigocliente=2;
+update cliente set cpfCliente= 547657543 where codigocliente=3;
+update cliente set cpfCliente=456743245 where codigocliente=4;
+update cliente set cpfCliente=564738904 where codigocliente=5;
+
+
+
+update cliente set datanascCliente='1967-05-30' where codigocliente=1;
+update cliente set datanascCliente='2000-12-21' where codigocliente=2;
+update cliente set datanascCliente='2007-04-15' where codigocliente=3;
+update cliente set datanascCliente='2008-08-08' where codigocliente=4;
+update cliente set datanascCliente='1965-03-11' where codigocliente=5;
